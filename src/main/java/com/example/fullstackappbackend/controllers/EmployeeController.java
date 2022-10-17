2772bfefa373550d4769a773ba9e1976490eb43a
Fhellipe Valentin <fhellipereis@outlook.com>
2022-10-17 15:32:17 -0300

    controladores e exception externo

--- src/main/java/com/example/fullstackappbackend/controllers/EmployeeController.java
+++ src/main/java/com/example/fullstackappbackend/controllers/EmployeeController.java
@@ -1,4 +1,72 @@
 package com.example.fullstackappbackend.controllers;
 
+import com.example.fullstackappbackend.exception.ResourceNotFoundException;
+import com.example.fullstackappbackend.models.Employee;
+import com.example.fullstackappbackend.repository.EmployeeRepository;
+import com.example.fullstackappbackend.service.SequenceGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// dizer que essa aplicação é um app cross-domain
+@CrossOrigin(origins = "http://localhost:4200")
+// dizer que essa classe será um restcontroller com a responsabilidade de fazer as requisicoes http
+@RestController("/api/v1")
 public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+    @GetMapping("/employess")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable(value = "id") Long employeeId )
+        throws ResourceNotFoundException {
+            Employee employee = employeeRepository.findById(employeeId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Colaborador não encontrado: " + employeeId));
+            return ResponseEntity.ok().body(employee);
+        }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@Valid @RequestBody Employee employee) {
+        employee.setId(sequenceGeneratorService.generateSequence(Employee.SEQUENCE_NAME));
+        return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+                                                   @Valid @RequestBody Employee employeeDetails)
+            throws ResourceNotFoundException{
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Colaborador não encontrado: " + employeeId));
+        // manipular valores a partir das propriedades definidas no model
+        employee.setName(employeeDetails.getName());
+        employee.setEmail(employeeDetails.getEmail());
+        employee.setPhone(employeeDetails.getPhone());
+        final Employee updateEmployee = employeeRepository.save(employee);
+
+        return ResponseEntity.ok(updateEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Colaborador não encontrado: " + employeeId));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("excluído", Boolean.TRUE);
+        return response;
+    }
 }
